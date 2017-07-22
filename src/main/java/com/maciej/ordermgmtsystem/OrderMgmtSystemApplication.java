421ce2efa6c87bf80d6ff6c23380a5554ee2337e
maciej <buczkowski.maciej@gmail.com>
2017-07-22 14:57:41 +0100

    Initial commit

---
+++ src/main/java/com/maciej/ordermgmtsystem/OrderMgmtSystemApplication.java
@@ -0,0 +1,12 @@
+package com.maciej.ordermgmtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMgmtSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maciej/ordermgmtsystem/OrderMgmtSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maciej.ordermgmtsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderMgmtSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
