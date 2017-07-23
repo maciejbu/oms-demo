3ce07eb40ee4677b3f5b0b5a023bf1a234e8c40c
maciej <username@users.noreply.github.com>
2017-07-23 19:49:18 +0100

    showing products

--- src/main/java/com/maciej/ordermgmtsystem/domain/Product.java
+++ src/main/java/com/maciej/ordermgmtsystem/domain/Product.java
@@ -1,11 +1,15 @@
 package com.maciej.ordermgmtsystem.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Product {
     private final String isin;
     private final String description;
 
     // Production system likely to use internal ID in addition to ISIN
-    public Product(String isin, String description) {
+    @JsonCreator
+    public Product(@JsonProperty("isin") String isin, @JsonProperty("description") String description) {
         this.isin = isin;
         this.description = description;
     }
--- src/test/java/com/maciej/ordermgmtsystem/integration/ProductIntegrationTest.java
+++ src/test/java/com/maciej/ordermgmtsystem/integration/ProductIntegrationTest.java
@@ -1,7 +1,11 @@
 package com.maciej.ordermgmtsystem.integration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maciej.ordermgmtsystem.domain.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,9 +13,14 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,6 +40,15 @@ public class ProductIntegrationTest {
     @Test
     @WithMockUser
     public void listProducts() throws Exception {
-        mockMvc.perform(get("/rest/products")).andExpect(status().is2xxSuccessful());
+        ResultActions resultActions = mockMvc.perform(get("/rest/products"));
+        MvcResult mvcResult = resultActions.andExpect(status().is2xxSuccessful()).andReturn();
+
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<Product> result = mapper.readValue(contentAsString, new TypeReference<List<Product>>() { });
+        assertFalse("received no products", result.isEmpty());
+        // I think assertions at this point are strong enough for this integration test as list of products is fixed
+        // for demonstration purposes. This could be expanded and products injected at the beginning of the test.
     }
 }
